f2656b26d4e14093e601b2a3998a7145348e3404
Zi Qing <qingacc@gmail.com>
2023-11-06 00:41:24 +0800

    Halfway animation
    facing script components adding issue

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,29 +5,57 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
 
+    private float dirX = 0f;
+    [SerializeField] private moveSpeed = 7f;
+    [SerializeField] private jumpForce = 14f;
 
-   // int wholeNumber = 16;
-   // float decimalNumber = 4.54f;
-   // string text = "blabla";
-   // bool boolean = false;
-
+    // either use [SerializeField] private float or make it public
+    // int wholeNumber = 16;
+    // float decimalNumber = 4.54f;
+    // string text = "blabla";
+    // bool boolean = false;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);    
 
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector3(rb.velocity.x, 14f);
+        }
+
+        UpdateAnimationState();
+    }
+
+    //private void neans just exist & execuate code, don't come out a result
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
